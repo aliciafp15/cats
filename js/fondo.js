0680b85d978ff35c29b5e6804e0a55dbb272dc6f
uo275727 <79641793+aliciafp15@users.noreply.github.com>
2023-12-13 18:23:45 +0100

    Add files via upload

--- js/fondo.js
+++ js/fondo.js
@@ -10,78 +10,45 @@ class Fondo{
     buscaFoto() {
         // Configura la URL de la API de Flickr
         const apiKey = '87cd7336be903190374dc6fef69b087f';
-        const flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        const flickrAPI = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+
 
         const conf = {
-            method: 'flickr.photos.search',
-            api_key: apiKey,
-            tags: 'nature',
-            format: 'json',
-            lat: this.latitud,
-            lon: this.longitud,
-            radius: 6,
-            text : 'Harare',
-            per_page: 1,
-            geo_context : 2, //outdoors
-            extras: 'landscape'
+            // method: 'flickr.photos.search',
+            "api_key": apiKey,
+            // tags: 'nature',
+            "lat": this.latitud, //problema poniendo this
+            "lon": this.longitud,
+            "format": 'json',
+            radius: 10,
+            // text : 'Zimbabue',
+            // per_page: 3,
+            "nojsoncallback": "1"
 
         };
 
 
         $.getJSON(flickrAPI,conf)
         .done(function (data) {
-            //seleccionar la foto
-            var fotoLink = data.items[0].media.m;
-            var betterQuality = fotoLink.replace('_m', '_b');
-            var url = `url('${betterQuality}')`
-            console.log(url)
+            var foto = data["photos"]["photo"][0];
+            var url = "https://live.staticflickr.com/" + foto["server"] + "/" + foto["id"]
+                    + "_" + foto["secret"] + "_b.jpg";
 
-            //establecer la imaegn paar que ocupe toda la pantalla
-            $('body').css('background-image', url)
-            .css('background-size', 'cover');
+
+            // // establecer la imaegn paar que ocupe toda la pantalla
+            // $('body').css('background-image', url)
+            // .css('background-size', 'cover');
+
+            $("body").css("background", "no-repeat url(" + url + ")  center center fixed")
+                    .css('background-size', 'cover');
             console.log('se cambió el fondo')
         })
         .fail(function (error) {
             console.error('Error en la solicitud AJAX:', error);
+            console.error('Error en la solicitud AJAX:', error.responseText);
+
         });
 
     }
-    
-    
 
-
-    //API PUBLICA
-    ponFondo(){
-        const flickrApiUrl = 'https://api.flickr.com/services/rest/';
-        const flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";//api anitgua
-
-
-        $.getJSON(flickrAPI, 
-                {
-                    tags : "nature",
-                    format: "json",
-                    lat: this.latitud,
-                    lon: -this.longitud,
-                    radius: 6,
-                    text: 'Harare',
-                    sort: 'relevance',
-                    per_page: 1 // Obtener solo una imagen
-                })
-            .done(function (data) {
-                //seleccionar la foto
-                var fotoLink = data.items[0].media.m;
-                var betterQuality = fotoLink.replace('_m', '_b');
-                var url = `url('${betterQuality}')`
-                console.log(url)
-
-                //establecer la imaegn paar que ocupe toda la pantalla
-                $('body').css('background-image', url)
-                .css('background-size', 'cover');
-                console.log('se cambió el fondo')
-            })
-            .fail(function (error) {
-                console.error('Error en la solicitud AJAX:', error);
-            });
-    
-    }
 }
