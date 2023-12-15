f818b36c984e4cbebe7791ea508bdd8876c957f2
uo275727 <79641793+aliciafp15@users.noreply.github.com>
2023-12-15 23:42:58 +0100

    Add files via upload

--- js/viajes.js
+++ js/viajes.js
@@ -1,9 +1,7 @@
 class Viajes{
 
-
-    constructor(){
-        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
-        // console.log(this.longitud);
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
     }
 
     getPosicion(posicion){
@@ -14,9 +12,11 @@ class Viajes{
         this.altitud          = posicion.coords.altitude;
         this.precisionAltitud = posicion.coords.altitudeAccuracy;
         this.rumbo            = posicion.coords.heading;
-        this.velocidad        = posicion.coords.speed;       
+        this.velocidad        = posicion.coords.speed;
+        this.getMapaEstaticoGoogle();    
+        
+        this.initMap();
     }
-
 
     verErrores(error){
         switch(error.code) {
@@ -35,36 +35,59 @@ class Viajes{
         }
     }
 
-    getLongitud(){
-        return this.longitud;
-    }
-    getLatitud(){
-        return this.latitud;
-    }
-    getAltitud(){
-        return this.altitud;
-    }
+    getMapaEstaticoGoogle(){
+        var apiKey = "&key=AIzaSyCf0Vt1Lv1MujAh-b-7HHRvwDl49DSRjhg"; //mia
+        // var apiKey = "&key=AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU"; //cueva
 
-
-    
-
-    mapaEstatico(){
-        var apiKey = "AIzaSyB0uO15eH5od3CuCnuOT-f13-8nWCJ9iI8";
         var url = "https://maps.googleapis.com/maps/api/staticmap?";
-
         var centro = "center=" + this.latitud + "," + this.longitud;
         //zoom (obligatorio si no hay marcadores)
         //zoom: 1 (el mundo), 5 (continentes), 10 (ciudad), 15 (calles), 20 (edificios)
         var zoom ="&zoom=10";
         var tamaño= "&size=800x600";
         var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
+  
         var sensor = "&sensor=false"; 
         
         this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + apiKey;
-        var mapaEst = $("<img src='"+this.imagenMapa+"' alt='mapa estático google' />");
-        $("main").append(mapaEst);
-
+        console.log(this.imagenMapa);
+        $("main").append("<img src='"+this.imagenMapa+"' alt='mapa estático google' />");
     }
-   
+
+
+
+
+    initMap(){  
+
+
+        var centro = {lat: this.latitud, lng: this.longitud};
+        var mapa = $('main').attr('id','mapa');
+        var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'),{
+        // var mapaGeoposicionado = new google.maps.Map($('mapa')[0],{
+
+            zoom: 8,
+            center:centro
+        });
+        
+        infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
+    
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Localización encontrada');
+                infoWindow.open(mapaGeoposicionado);
+                mapaGeoposicionado.setCenter(pos);
+              }, function() {
+                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+              });
+            } else {
+              // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+            }
+          }
 
 }
