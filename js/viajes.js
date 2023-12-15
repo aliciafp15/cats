70c71f0df56b7ce9ae6545982316a6e1ce682fb8
uo275727 <79641793+aliciafp15@users.noreply.github.com>
2023-12-15 21:55:00 +0100

    Add files via upload

---
+++ js/viajes.js
@@ -0,0 +1,70 @@
+class Viajes{
+
+
+    constructor(){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+        // console.log(this.longitud);
+    }
+
+    getPosicion(posicion){
+        this.mensaje = "Se ha realizado correctamente la petición de geolocalización";
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+
+
+    verErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+            break;
+        }
+    }
+
+    getLongitud(){
+        return this.longitud;
+    }
+    getLatitud(){
+        return this.latitud;
+    }
+    getAltitud(){
+        return this.altitud;
+    }
+
+
+    
+
+    mapaEstatico(){
+        var apiKey = "AIzaSyB0uO15eH5od3CuCnuOT-f13-8nWCJ9iI8";
+        var url = "https://maps.googleapis.com/maps/api/staticmap?";
+
+        var centro = "center=" + this.latitud + "," + this.longitud;
+        //zoom (obligatorio si no hay marcadores)
+        //zoom: 1 (el mundo), 5 (continentes), 10 (ciudad), 15 (calles), 20 (edificios)
+        var zoom ="&zoom=10";
+        var tamaño= "&size=800x600";
+        var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
+        var sensor = "&sensor=false"; 
+        
+        this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + apiKey;
+        var mapaEst = $("<img src='"+this.imagenMapa+"' alt='mapa estático google' />");
+        $("main").append(mapaEst);
+
+    }
+   
+
+}
