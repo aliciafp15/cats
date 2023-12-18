1a92d85c5339048ed514af498e99ed7766e7c384
uo275727 <79641793+aliciafp15@users.noreply.github.com>
2023-12-18 18:06:31 +0100

    Add files via upload

--- viajes.php
+++ viajes.php
@@ -1,3 +1,57 @@
+<?php
+class Carrusel
+{
+
+  private $capital;
+  private $pais;
+
+
+  public function __construct($capital, $pais)
+  {
+    $this->capital = $capital;
+    $this->pais = $pais;
+  }
+
+
+  function obtenerImagenes()
+  {
+      $apiKey = '87cd7336be903190374dc6fef69b087f';
+      $flickrAPI = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+  
+      $params = array(
+          'api_key' => $apiKey,
+          'tags' => $this->pais . ',' . $this->capital,
+          'format' => 'json',
+          'per_page' => 10,
+          'nojsoncallback' => '1',
+          'sort' => 'relevance',  // Ordenar por relevancia
+      );
+  
+      $url = $flickrAPI . '&' . http_build_query($params);
+      $respuesta = file_get_contents($url);
+      $data = json_decode($respuesta, true);
+  
+      $carrusel = "<article data-element='carrusel'><h3>Carrusel de Zimbabue</h3>";
+      foreach ($data["photos"]["photo"] as $foto) {
+          $titulo = $foto["title"];
+          $URLfoto = "https://live.staticflickr.com/" . $foto["server"] . "/" . $foto["id"] . "_" . $foto["secret"] . "_b.jpg";
+          $img = "<img data-element='carruselImg' alt='" . $titulo . "' src='" . $URLfoto . "' />";
+          $carrusel .= $img;
+      }
+      $carrusel .= "<button onclick='v.fotoSiguiente()' data-action='next'> > </button>
+      <button data-action='prev' onclick='v.fotoAnterior()'> < </button></article>";
+  
+      return $carrusel;
+  }
+  
+
+}
+
+// $carrusel = new Carrusel($capital, $pais);
+?>
+
+
+
 <!DOCTYPE html>
 
 <html lang="es">
@@ -8,13 +62,14 @@
   <title>Escritorio Virtual - Viajes</title>
   <meta name="author" content="Alicia Fernández Pushkina" />
   <meta name="description" content="aquí cada documento debe tener la  descripción del contenido concreto del mismo" />
-  <meta name="keywords"
-    content="aquí cada documento debe tener la lista de las palabras clave del mismo separadas por comas" />
+  <meta name="keywords" content="aquí cada documento debe tener la lista de las palabras clave del mismo separadas por comas" />
   <!-- Definir la ventana gráfica -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" type="text/css" href="estilo/estilo.css" />
   <link rel="stylesheet" type="text/css" href="estilo/layout.css" />
   <link rel="stylesheet" type="text/css" href="estilo/viajes.css" />
+  <link rel="stylesheet" type="text/css" href="estilo/carrusel.css" />
+
   <link href="multimedia/imagenes/favicon.ico" rel="icon" />
 
   <!--inicio imports para el mapa dinamico-->
@@ -22,8 +77,7 @@
   <link href='https://api.mapbox.com/mapbox-gl-js/v3.0.1/mapbox-gl.css' rel='stylesheet' />
   <!--fin imports para el mapa dinamico-->
 
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> <!--jquery min-->
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> <!--jquery min-->
   <script src="js/viajes.js"></script>
 
 
@@ -50,6 +104,11 @@
 
   <main>
     <h2>Viajes</h2>
+    <!-- meter el carrusel -->
+    <?php
+    $carrusel = new Carrusel('Harare', 'Zimbabue'); //Zimbabwe 
+    echo $carrusel->obtenerImagenes(); ?>
+
     <section id="map">
       <!-- mapa dinamico -->
     </section>
@@ -68,7 +127,7 @@
       <section id="mapPlanimetria">
         <!-- mapa dinamico para la planimetria -->
       </section>
-      
+
     </section>
 
 
@@ -81,7 +140,6 @@
 
   <script>
     var v = new Viajes();
-
   </script>
 </body>
 
